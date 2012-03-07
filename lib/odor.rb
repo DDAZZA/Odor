93e54466d9f8d9869f14e25ccebda9e150b6065a
Dave Elliott <ddazza@gmail.com>
2012-03-07 21:36:45 +0000

    Added initial logic
    extracts changed line numbers from git diff and does a cross comparison with
    the rails best practices output

--- lib/odor.rb
+++ lib/odor.rb
@@ -10,10 +10,52 @@ module Odor
   end
 
   def self.check_file(filename)
+    line_numbers = line_changes(filename)
+    issues = %x[rails_best_practices -f text #{filename} --silent]
+    issues = issues.split("\n")
+
+    issues.each do |hint|
+      line_numbers.each do |num|
+        if hint.include?(num.to_s)
+          puts hint
+          break
+        end
+      end
+    end
+    return true
+  end
+
+  def self.line_changes(filename)
     changes = %x[git diff -U0 #{filename}]
-    
-    puts changes.split("\n")
-    issues = %x[rails_best_practices -f text #{filename}]
-    puts issues   
+    changes = changes.split("\n")
+    changes.shift(4)
+    #changes = changes.map{ |c| c if !c.nil? && c.start_with?('@') }
+
+    line_changes = []
+    changes.each do |c|
+      line_changes << c if !c.nil? && c.start_with?('@')
+    end
+
+    lines = []
+
+    line_changes.each do |s|
+      s = s.split
+
+      s.shift(2)
+      li = s.first[1, 50].split(',')
+
+      li.last.to_i.times do |i|
+        lines << li.first.to_i + i
+      end
+    end
+
+#    s = changes.first.split
+#    s.shift(2)
+#    li = s.first[1, 50].split(',')
+#
+#    li.last.to_i.times do |i|
+#      lines << li.first.to_i + i
+#    end
+    return lines
   end
 end
