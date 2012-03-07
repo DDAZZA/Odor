578ab8516e8f73fc78c074309886719c9767638a
Dave Elliott <ddazza@gmail.com>
2012-03-07 19:21:47 +0000

    added bash commands

--- lib/odor.rb
+++ lib/odor.rb
@@ -2,13 +2,18 @@ require "odor/version"
 
 module Odor
   def self.run(*args)
-    changed_files = %x[git status -s #{path}]
+    changed_files = %x[git status -s]
+    changed_files.split("\n").each do |line|
+      line = line.split
+      check_file(line.last)
+    end
   end
 
-  def check_file(filename)
+  def self.check_file(filename)
     changes = %x[git diff -U0 #{filename}]
-    puts changes
+    
+    puts changes.split("\n")
     issues = %x[rails_best_practices -f text #{filename}]
-    puts issues
+    puts issues   
   end
 end
